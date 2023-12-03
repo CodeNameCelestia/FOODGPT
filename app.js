c461b9064705ae1ae82244067c15d3cad76019ff
Alejandro <Alecayasa15@gmail.com>
2023-12-03 14:42:37 +0800

    Food bot

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var indexRouter = require("./routes/index.js");
 const { auth } = require("express-openid-connect");
 require("dotenv").config();
-
+const bodyParser = require('body-parser');
+const FoodBot = require('./services/openaiService.js');
 
 const config = {
     authRequired: false,
@@ -21,11 +22,25 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.use(bodyParser.json());
+
+
+
+const foodBot = new FoodBot("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");
+
+app.post('/chat', async (req, res) => {
+  const { user_input } = req.body;
+  const chatGPTReply = await foodBot.getFoodRecommendation(user_input);
+  res.json({ reply: chatGPTReply });
+});
 
 
 //auth
 app.use(auth(config));
 
+app.get('/foodbot', (req, res) => {
+  res.render('foodbot'); // Renders views/foodbot.ejs
+});
 
 app.use('/', indexRouter);
 
---
+++ services/openaiService.js
@@ -0,0 +1,35 @@
+const OpenAI = require('openai');
+
+class FoodBot {
+  constructor(apiKey) {
+    this.openai = new OpenAI({ apiKey });
+    this.messages = [
+      {
+        role: 'system',
+        content:
+          'You are a well-renowned chef in the Philippines who has well-versed skills in the art of cooking Filipino cuisines. You have cooked almost every dish found in the Philippines, and most of all, you can recommend the right dishes fit for the occasion, situation, circumstance, and the available ingredients. You can also edit the recipe when the user has limited ingredients.\n\nThere is a chance where you are provided with their mood and situation, your task here is to find a dish that fits with their current mood and situation and recommend them that dish.\n\nNote that the dish recommended must have ingredients that are common in the Philippines.\n\nAfter recommending the dish, you must ask "Do you want the full recipe?"\n',
+      },
+    ];
+  }
+
+  async getFoodRecommendation(userInput) {
+    this.messages.push({ role: 'user', content: userInput });
+
+    const response = await this.openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: this.messages,
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const chatGPTReply = response.choices[0].message.content;
+    this.messages.push({ role: 'assistant', content: chatGPTReply });
+
+    return chatGPTReply;
+  }
+}
+
+module.exports = FoodBot;
